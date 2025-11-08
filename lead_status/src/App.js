d8c156e7df05304010027b4e16478f3b936e97dc
rajputmanasvi <rajputmanasvi2008@gmail.com>
2025-11-08 04:00:09 +0000

    updated

--- lead_status/src/App.js
+++ lead_status/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import LeadsStatusChart from "./components/LeadsStatusChart";
-import LeadsStatusTable from "./components/LeadsStatusTable";
-
+// import LeadsStatusChart from "./components/LeadsStatusChart";
+// import LeadsStatusTable from "./components/LeadsStatusTable";
+import LeadStatusList from "./components/LeadStatusList";
 export default function App() {
   return (
     <div className="min-h-screen bg-[#e5e9ec]">
       <div className="max-w-7xl mx-auto space-y-2 py-4">
         {/* Chart Section */}
         <div className="rounded-lg  p-3">
-          <LeadsStatusChart />
+          {/* <LeadsStatusChart /> */}
         </div>
-
+<LeadStatusList/>
         {/* Table Section */}
         <div className="rounded-lg  p-3">
-          <LeadsStatusTable />
+          {/* <LeadsStatusTable /> */}
         </div>
       </div>
     </div>
